c797791fd97e08cdbc665dd5193058f2180d8c87
dharmishtha <114208599+dharmishthamakwana@users.noreply.github.com>
2022-12-13 19:37:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+        ---------------------------------------------------task1------------------------------
+
+
 import 'package:flutter/material.dart';
 void main()
 {
@@ -22,7 +25,7 @@ void main()
                   height: 40,
                   width: 100,
                   decoration: BoxDecoration(
-                      //borderRadius: BorderRadius.all(Radius.circular(30)),
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
                       gradient: LinearGradient(
                           colors: [
                             Colors.blue,
@@ -38,3 +41,283 @@ void main()
       )
   );
 }
+
+
+----------------------------task2------------------------------------
+  
+  
+  import 'package:flutter/material.dart';
+void main()
+{
+  int i=0;
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Custom Button 2"),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: (){
+                  i++;
+                  print("$i");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("Secondary",style: TextStyle(color: Colors.white,fontSize: 18)),
+                  height: 40,
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                       borderRadius: BorderRadius.all(Radius.circular(5)),
+                       gradient: LinearGradient(
+                           colors: [
+                             Colors.purple,
+                             Colors.purpleAccent.shade100,
+                           ]
+                       )
+                   ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
+
+----------------------------------task3--------------------------------------
+  
+  
+  
+  import 'package:flutter/material.dart';
+void main()
+{
+  int i=0;
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Custom Button 3"),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: (){
+                  i++;
+                  print("$i");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("Secondary",style: TextStyle(color: Colors.white,fontSize: 18)),
+                  height: 40,
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                       borderRadius: BorderRadius.all(Radius.circular(5)),
+                       gradient: LinearGradient(
+                           colors: [
+                             Colors.green,
+                             Colors.green.shade200,
+                           ]
+                       )
+                   ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
+
+
+
+---------------------------task4-------------------------
+  
+  
+  import 'package:flutter/material.dart';
+void main()
+{
+  int i=0;
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Custom Button 4"),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: (){
+                  i++;
+                  print("$i");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("Success",style: TextStyle(color: Colors.white,fontSize: 18)),
+                  height: 40,
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                       borderRadius: BorderRadius.all(Radius.circular(5)),
+                       gradient: LinearGradient(
+                           colors: [
+                             Colors.pink,
+                             Colors.pink.shade200,
+                           ]
+                       )
+                   ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
+
+
+-----------------------task5------------------------------
+  
+  
+  
+  import 'package:flutter/material.dart';
+void main()
+{
+  int i=0;
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Custom Button 5"),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: (){
+                  i++;
+                  print("$i");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("primary",style: TextStyle(color: Colors.white,fontSize: 18)),
+                  height: 40,
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                       borderRadius: BorderRadius.all(Radius.circular(50)),
+                       gradient: LinearGradient(
+                           colors: [
+                             Colors.blue,
+                             Colors.blue.shade200,
+                           ]
+                       )
+                   ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
+
+-------------------task6----------------------------
+  
+  import 'package:flutter/material.dart';
+void main()
+{
+  int i=0;
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Custom Button 6"),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: (){
+                  i++;
+                  print("$i");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("secondary",style: TextStyle(color: Colors.white,fontSize: 18)),
+                  height: 40,
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                       borderRadius: BorderRadius.all(Radius.circular(50)),
+                       gradient: LinearGradient(
+                           colors: [
+                             Colors.purple,
+                             Colors.purple.shade200,
+                           ]
+                       )
+                   ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
+
+
+
+
+
+---------------------------task7-------------------------------
+  
+  
+  import 'package:flutter/material.dart';
+void main()
+{
+  int i=0;
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Custom Button 7"),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: (){
+                  i++;
+                  print("$i");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("scuccess",style: TextStyle(color: Colors.white,fontSize: 18)),
+                  height: 40,
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                       borderRadius: BorderRadius.all(Radius.circular(50)),
+                       gradient: LinearGradient(
+                           colors: [
+                             Colors.green,
+                             Colors.green.shade200,
+                           ]
+                       )
+                   ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
+
